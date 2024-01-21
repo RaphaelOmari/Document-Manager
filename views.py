29aa2a30227a10b079e69b9b79fc4e51a8f2b39e
RaphaelOmari <127858530+RaphaelOmari@users.noreply.github.com>
2024-01-21 13:46:44 +0000

    ii_processi_eva

--- views.py
+++ views.py
@@ -1,4 +1,8 @@
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.contrib.auth.decorators import login_required
+from .models import File, FileRevision
+import bcrypt 
 
 def validate_file_type(upload):
     allowed_mime_types = ['application/pdf', 'image/jpeg']
@@ -9,3 +13,19 @@ def validate_file_size(upload):
     max_size = 1024*1024*5
     if upload.size > max_size:
         raise ValidationError('File too large. Size should not exceed 5 MB.')
+    
+@login_required
+def upload_file(request):
+    if request.method == 'POST':
+        file = request.FILES['file']
+        url = request.POST['url']
+        try:
+            validate_file_type(file)
+            validate_file_size(file)
+
+        except ValidationError as e:
+            return HttpResponse(e.messages, status=400)
+        except Exception as e:
+            print(e)
+
+    return render(request, 'upload.html')
